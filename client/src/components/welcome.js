a33c963f68c87686cdb71515395ca39b9c2ea924
Haichao Yu <haichaoy@asu.edu>
2017-09-14 13:30:10 -0700

    Update welcome page.

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,31 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => (
   <div>
 
     { /*Main jumbotron for a primary marketing message or call to action*/ }
     <div className="jumbotron">
-      <h1 className="display-3">Hello, world!</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-      <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      <h1 className="display-3">Welcome!</h1>
+      <p>This is a MERN stack based fully functioning blog system. Here, you can share your experience and ideas with other people.</p>
+      <p><Link className="btn btn-primary btn-lg" to="/posts" role="button">Look the blog posts &raquo;</Link></p>
     </div>
 
     { /*Example row of columns*/ }
-    <div className="row">
+    <div className="row text-justify">
       <div className="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <h2>Front-end</h2>
+        <p>The front-end client is built as a simple-page-application using React and Redux (for middlewares and reducers). Besides, React-Router is used for navigation. Redux-Thunk is used for processing asynchronous requests. Bootstrap 4 is used for page styling.</p>
       </div>
       <div className="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <h2>Back-end</h2>
+        <p>The back-end server is built with Express.js and Node.js in MVC pattern, which provides completed REST APIs for data interaction. Passport.js is used as an authentication middleware in the sever. JSON Web Token (JWT) is used for signing in user and making authenticated requests.</p>
       </div>
       <div className="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <h2>Database</h2>
+        <p>MongoDB is used as the back-end database, which include different data models/schemas (i.e., User, Post and Comment). Mongoose is used to access the MongoDB for CRUD actions (create, read, update and delete).</p>
       </div>
     </div>
 
